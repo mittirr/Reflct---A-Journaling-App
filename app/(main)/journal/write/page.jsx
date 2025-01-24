87afeb03bb388b4f8e6413b558369862043b0863
mittir <akashjeet9038@gmail.com>
2025-01-24 19:22:26 +0530

    update journal write page

--- app/(main)/journal/write/page.jsx
+++ app/(main)/journal/write/page.jsx
@@ -9,13 +9,14 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { journalSchema } from '@/app/lib/schema';
 import { Input } from '@/components/ui/input';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
-import { MOODS } from '@/app/lib/moods';
+import { getMoodById, MOODS } from '@/app/lib/moods';
 import { object } from 'zod';
+import { Button } from '@/components/ui/button';
 
 const ReactQuill = dynamic(() => import("react-quill-new"), {ssr: false});
 const JournalEntryPage = () => {
 
-  const {register, handleSubmit, control, formState:{errors}} = useForm({
+  const {register, handleSubmit, control, formState:{errors}, getValues} = useForm({
     resolver: zodResolver(journalSchema), 
     defaultValues:{
       title: "",
@@ -27,10 +28,14 @@ const JournalEntryPage = () => {
 
   const isLoading = false;
 
+  const onSubmit = handleSubmit(async (data) => {
+    console.log(data)
+  });
+
   return (
 
     <div className="py-8">
-      <form className="space-y-2 mx-auto">
+      <form className="space-y-2 mx-auto" onSubmit={onSubmit}>
         <h1 className="text-5xl md:text-6xl gradient-title">
           what&apos;s on your mind?
         </h1>
@@ -40,6 +45,7 @@ const JournalEntryPage = () => {
         <div className="space-y-2">
           <label className="text-sm font-medium">Title</label>
           <Input 
+            disables={isLoading}
             {...register("title")}
             placeholder="Enter a Title..."
             className={`py-5 md:text-md ${errors.title?"border-red-500" : ""}`}
@@ -78,6 +84,55 @@ const JournalEntryPage = () => {
           {errors.mood && (<p className="text-red-500 text-sm">{errors.mood.message}</p>)}
           
           
+        </div>
+
+        <div className="space-y-2">
+          <label className="text-sm font-medium">
+            {getMoodById(getValues("mood"))?.prompt ?? "write your thoughts..."}
+          </label>
+          <Controller
+            name="content"
+            control={control}
+            render={({field}) => <ReactQuill 
+            readOnly={isLoading} 
+            theme="snow" 
+            value={field.value} 
+            onChange={field.onChange}
+            modules={{
+              toolbar:[
+                [{header: [1,2,3,false]}],
+                ["bold", "italic", "underline", "strike"],
+                [{list: "ordered"}, {list: "bullet"}],
+                ["blockquote", "code-block"],
+                ["link"],
+                ["clean"],
+              ],
+            }}
+            />}
+          />
+
+          {errors.content && (<p className="text-red-500 text-sm">{errors.content.message}</p>)}
+          
+          
+        </div>
+
+        <div className="space-y-2">
+          <label className="text-sm font-medium">
+            Add to Collection (optional)
+          </label>
+          {/* <Controller
+            name="content"
+            control={control}
+            render={({field}) => {}}
+          /> */}
+
+          {errors.collectionId && (<p className="text-red-500 text-sm">{errors.collectionId.message}</p>)}
+          
+          
+        </div>
+
+        <div className="space-y-4 flex">
+          <Button type="submit" variant="journal">Publish</Button>
         </div>
       </form>
     </div>
