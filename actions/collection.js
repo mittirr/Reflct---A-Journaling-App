373dc030bd6a06eed53480d5b9529d7e70b0667c
mittir <akashjeet9038@gmail.com>
2025-01-29 19:31:01 +0530

    collection action create

--- actions/collection.js
+++ actions/collection.js
@@ -1,18 +1,11 @@
-"use server"
-
-import { MOODS } from "@/app/lib/moods";
-import { getPixabayImage } from "./public";
-import { revalidatePath } from "next/cache";
-import { auth } from "@clerk/nextjs/server";
-import { db } from "../lib/prisma";
-import { Code } from "lucide-react";
-import { request } from "@arcjet/next";
 import aj from "@/lib/arcjet";
+import { db } from "@/lib/prisma";
+import { request } from "@arcjet/next";
+import { auth } from "@clerk/nextjs/server";
+import { revalidatePath } from "next/cache";
 
-
-export async function createJournalEntry (data) {
+export async function createCollection(data) {
     try {
-
         const {userId} = await auth();                       // chechking if user is logged in or not and acting accordingly
         if(!userId) throw new Error("Unauthorized");
 
@@ -42,7 +35,7 @@ export async function createJournalEntry (data) {
 
             throw new Error("Request Blocked");
         }
-        
+
         const user = await db.user.findUnique({                    // if user exists inside database
             where:{ clerkUserId: userId },
         })
@@ -50,35 +43,44 @@ export async function createJournalEntry (data) {
 
         if (!user){                                          // if user is not found in database then throw error
             throw new Error("No User  Found");
-        }
+        } 
 
-        const mood = MOODS[data.mood.toUpperCase()];         
-        if(!mood) throw new Error("Invalid mood");
-
-        const moodImageUrl = await getPixabayImage(data.moodQuery);         // sending api request to pixabay to get image according to mood
-
-        const entry = await db.entry.create({                               // creating an entry to database
+        const collection = await db.collection.create({
             data:{
-                title: data.title,
-                content: data.content,
-                mood: mood.id,
-                moodScore: mood.score,
-                moodImageUrl,
+                name: data.name,
+                description: data.description,
                 userId: user.id,
-                collectionId: data.collectionId || null,
             },
-        })
+        });
 
-        await db.draft.deleteMany({                                         // deleting draft after it is saved to main collection
-            where:{
-                userId: user.id,
-            }
-        })
+        revalidatePath("/dashboard");
+        return collection;
 
-        revalidatePath('/dashboard');
-
-        return entry;
-     } catch (error) {
+    } catch (error) {
         throw new Error(error.message);
     }
 }
+
+
+export async function getCollection() {
+        const {userId} = await auth();                       // chechking if user is logged in or not and acting accordingly
+        if(!userId) throw new Error("Unauthorized");
+
+
+        const user = await db.user.findUnique({                    // if user exists inside database
+            where:{ clerkUserId: userId },
+        })
+
+        if (!user){                                          // if user is not found in database then throw error
+            throw new Error("No User  Found");
+        } 
+
+        const collections = await db.collection.findMany({
+            where:{
+                userId: user.id,
+            },
+            orderBy:{ createdAt: "desc" },
+        });
+
+        return collections;
+};
