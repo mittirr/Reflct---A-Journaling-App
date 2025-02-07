144edb7dea6eb90a6da8d04344b4fc810eb4a248
mittir <akashjeet9038@gmail.com>
2025-02-07 11:38:58 +0530

    fix collection-dialog popup

--- components/collection-dialog.jsx
+++ components/collection-dialog.jsx
@@ -5,14 +5,16 @@ import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, Di
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { BarLoader } from 'react-spinners'
-import { Textarea } from './ui/textarea'
-import { Button } from './ui/button'
 import { CollectionSchema } from '@/app/lib/schema'
+import { Textarea } from '@/components/ui/textarea'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+
 
 const CollectionForm = ({onSuccess, open, setOpen, loading}) => {
 
 
-    const {register, handlesubmit, formState: {errors},} = useForm({
+    const {register, handleSubmit, formState: {errors},} = useForm({
         resolver: zodResolver(CollectionSchema),
         defaultValues:{
             name: "",
@@ -21,47 +23,51 @@ const CollectionForm = ({onSuccess, open, setOpen, loading}) => {
     });
 
 
-    const onSubmit = handlesubmit(async(data)=>{
+    const onSubmit = handleSubmit(async (data) => {
         onSuccess(data);
     });
     
   return (
     <Dialog open={open} onOpenChange={setOpen}>
-    <DialogContent>
-        <DialogHeader>
-        <DialogTitle>Create New Collection</DialogTitle>
-        {loading && <BarLoader color="orange" width={"100%"}/>}
-            <form onsubmit={onsubmit} className="space-y-2">
-            <div className="space-y-2">
-            <label className="text-sm font-medium">Collection Name</label>
-            <Input 
-                disables={loading}
-                {...register("name")}
-                placeholder="Enter collection name..."
-                className={`${errors.title?"border-red-500" : ""}`}
-            />
-            {errors.name && (<p className="text-red-500 text-sm">{errors.name.message}</p>)}
-          
-            </div>
+        <DialogContent>
+            <DialogHeader>
+                <DialogTitle>Create New Collection</DialogTitle>
+            </DialogHeader>
+            
+            {loading && (
+                <BarLoader className="mb-4" color="orange" width={"100%"}/>
+                )}
 
-            <div className="space-y-2">
-            <label className="text-sm font-medium">Collection Name</label>
-            <Textarea 
-                disables={loading}
-                {...register("description")}
-                placeholder="Describe your collection..."
-                className={`${errors.title?"border-red-500" : ""}`}
-            />
-            {errors.description && (<p className="text-red-500 text-sm">{errors.description.message}</p>)}
+            <form onSubmit={onSubmit} className="space-y-6">
+                <div className="space-y-2">
+                    <label className="text-sm font-medium">Collection Name</label>
+                    <Input 
+                        {...register("name")}
+                        placeholder="Enter collection name..."
+                        className={`${errors.name?"border-red-500" : ""}`}
+                    />
+                    {errors.name && 
+                    (<p className="text-red-500 text-sm">{errors.name.message}</p>)}
           
-            </div>
+                </div>
 
-            <div className="flex justify-end gap-4">
-                <Button type="button" variant="ghost" onClick={() => setOpen(false)}>Cancel</Button>
-                <Button type="submit" variant="journal">Create Collection</Button>
-            </div>
+                <div className="space-y-2">
+                    <label className="text-sm font-medium">Collection Name</label>
+                    <Textarea 
+                        disables={loading}
+                        {...register("description")}
+                        placeholder="Describe your collection..."
+                        className={`${errors.description?"border-red-500" : ""}`}
+                    />
+                    {errors.description && (<p className="text-red-500 text-sm">{errors.description.message}</p>)}
+            
+                </div>
+
+                <div className="flex justify-end gap-4">
+                    <Button type="button" variant="ghost" onClick={() => setOpen(false)}>Cancel</Button>
+                    <Button type="submit" variant="journal">Create Collection</Button>
+                </div>
             </form>
-        </DialogHeader>
     </DialogContent>
     </Dialog>
 
