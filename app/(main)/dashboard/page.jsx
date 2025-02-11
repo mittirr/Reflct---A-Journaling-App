6779102f858c1297cabf9330a017e615fe617ffd
mittir <akashjeet9038@gmail.com>
2025-02-11 21:51:01 +0530

    server action call in Dashboard

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,9 +1,14 @@
+import { getCollections } from '@/actions/collection';
+import { getJournalEntries } from '@/actions/journal';
 import React from 'react'
 
-const Dashboard = () => {
+const Dashboard = async () => {
+
+  const collections = await getCollections();
+  const entriesData = await getJournalEntries();
   return (
     <div>Dashboard</div>
   )
-}
+};
 
 export default Dashboard
