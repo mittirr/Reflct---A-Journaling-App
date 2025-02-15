6ea849f34738ff2fc1e8c00e9eecbb86ca5faad8
mittir <akashjeet9038@gmail.com>
2025-02-15 21:31:27 +0530

    update dashboard

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -6,24 +6,27 @@ const Dashboard = async () => {
 
   const collections = await getCollections();
   const entriesData = await getJournalEntries();
-
-  console.log(collections,entriesData);
   
-  // const entriesByCollection =entriesData?.data.entries.reduce(
-  //   (acc,entry) => {
-  //     const collectionId = entry.collectionId || "unorganized";
+  const entriesByCollection =entriesData?.data.entries.reduce(
+    (acc,entry) => {
+      const collectionId = entry.collectionId || "unorganized";
 
-  //     if (!acc[collectionId]){
-  //       acc[collectionId] = [];
-  //     }
-  //     acc[collectionId].push(entry);
-  //     return acc;
-  //   },{});
+      if (!acc[collectionId]){
+        acc[collectionId] = [];
+      }
+      acc[collectionId].push(entry);
+      return acc;
+    },{});
 
-  //   console.log(entriesByCollection)
-  return (
-    <div>Dashboard</div>
-  )
+    console.log(entriesByCollection)
+  return <div className="px-4 py-8 space-y-8">
+      <section className="space-y-4">{/*Mood Analytics*/}</section>
+      
+      <Collections
+        collections={collections}
+        entriesByCollection={entriesByCollection}
+      />
+    </div>
 };
 
 export default Dashboard
