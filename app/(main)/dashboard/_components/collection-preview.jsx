ba93ce39bb4cef47370a3d1b308c3cfd3c8e2d28
mittir <akashjeet9038@gmail.com>
2025-02-18 13:03:16 +0530

    add organized and unorganized collections folderTab

--- app/(main)/dashboard/_components/collection-preview.jsx
+++ app/(main)/dashboard/_components/collection-preview.jsx
@@ -1,25 +1,28 @@
+import { Plus } from 'lucide-react';
+import Link from 'next/link';
 import React from 'react'
 
-const colorSchemes = {
-    unorganized: {
-      bg: "bg-amber-100 hover:bg-amber-50",
-      tab: "bg-amber-200 group-hover:bg-amber-300",
-    },
-    collection: {
-      bg: "bg-blue-100 hover:bg-blue-50",
-      tab: "bg-blue-200 group-hover:bg-blue-300",
-    },
-    createCollection: {
-      bg: "bg-gray-200 hover:bg-gray-100",
-      tab: "bg-gray-100 hover:bg-gray-50",
-    },
+  const colorSchemes = {
+      unorganized: {
+        bg: "bg-amber-100 hover:bg-amber-50",
+        tab: "bg-amber-200 group-hover:bg-amber-300",
+      },
+      collection: {
+        bg: "bg-blue-100 hover:bg-blue-50",
+        tab: "bg-blue-200 group-hover:bg-blue-300",
+      },
+      createCollection: {
+        bg: "bg-gray-200 hover:bg-gray-100",
+        tab: "bg-gray-100 hover:bg-gray-50",
+      },
   };
   
 
   const FolderTab = ({colorClass}) => (
     <div className={`absolute inset-x-4 -top-2 h-2 rounded-t-md transform -skew-x-6 transition-colors ${colorClass}`}
     />
-  )
+  );
+
 const CollectionPreview = ({
   id,
   name,
@@ -30,13 +33,31 @@ const CollectionPreview = ({
 }) => {
 
   if(isCreateNew){
-    return <button>
-      <FolderTab/>
+    return <button 
+    onClick={onCreateNew}
+    className="relative group h-[200px] cursor-pointer">
+      <FolderTab colorClass={colorSchemes["createCollection"].bg}/>
+      <div className={`relative h-full rounded-lg p-6 shadow-md hover:shadow-lg transition-all flex flex-col items-center justify-center gap-4 ${colorSchemes["createCollection"].tab}`}>
+        <div className="h-12 w-12 rounded-full bg-gray-200 group-hover:bg-gray-300 flex items-center justify-center">
+          <Plus className="h-6 w-6 text-gray-600"/>
+        </div>
+          <p className="text-gray-600 font-medium">Create New Collection</p>
+      </div>
     </button>
   }
   return (
-    <div>CollectionPreview</div>
-  )
-}
+    <Link 
+      href={`/collection/${isUnorganized? "unorganized" : id}`}
+      className="group relative"
+    >
+      <FolderTab 
+        colorClass={
+          colorSchemes[isUnorganized? "unorganized" : "collection"].tab
+          }
+      />
+      
+    </Link>
+  );
+};
 
 export default CollectionPreview
