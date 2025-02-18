0b0a9944a78db6d0bd1d097a7994541e4f8906fa
mittir <akashjeet9038@gmail.com>
2025-02-18 10:42:25 +0530

    add colorschems of collection preview

--- app/(main)/dashboard/_components/collection-preview.jsx
+++ app/(main)/dashboard/_components/collection-preview.jsx
@@ -1,6 +1,22 @@
 import React from 'react'
 
 const CollectionPreview = () => {
+
+  const colorSchemes = {
+    unorganized: {
+      bg: "bg-amber-100 hover:bg-amber-50",
+      tab: "bg-amber-200 group-hover:bg-amber-300",
+    },
+    collection: {
+      bg: "bg-blue-100 hover:bg-blue-50",
+      tab: "bg-blue-200 group-hover:bg-blue-300",
+    },
+    createCollection: {
+      bg: "bg-gray-200 hover:bg-gray-100",
+      tab: "bg-gray-100 hover:bg-gray-50",
+    },
+  };
+  
   return (
     <div>CollectionPreview</div>
   )
