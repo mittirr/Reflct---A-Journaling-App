be5eeb59a5bbf6c83ba5583135ea68e8ea23f882
mittir <akashjeet9038@gmail.com>
2025-02-20 17:16:07 +0530

    add timeOptions

--- app/(main)/dashboard/_components/mood-analytics.jsx
+++ app/(main)/dashboard/_components/mood-analytics.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 
+
+const timeOptions = [
+    { value: "7d", label: "Last 7 Days" },
+    { value: "15d", label: "Last 15 Days" },
+    { value: "30d", label: "Last 30 Days" },
+  ];
+
+  
 const MoodAnalytics = () => {
-  return (
-    <div>MoodAnalytics</div>
-  )
+    
+  return <div>MoodAnalytics</div>
 }
 
 export default MoodAnalytics
