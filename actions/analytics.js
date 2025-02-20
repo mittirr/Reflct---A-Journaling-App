74d2d9d0607fc41a12269eb28554dec348221f79
mittir <akashjeet9038@gmail.com>
2025-02-21 00:12:51 +0530

    create async function getAnalytics

--- actions/analytics.js
+++ actions/analytics.js
@@ -1,5 +1,15 @@
 "use server";
 
-export async function getAnalytics(params) {
-    
+import { db } from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/server";
+
+export async function getAnalytics(period = "30d") {
+    const { userId } = await auth();
+    if(!userId) throw new Error("unauthorized");
+
+    const user = await db.user.findUnique({
+        where:{ clerkUserId: userId},
+    });
+
+    if(!user) throw new Error("User not found");
 }
