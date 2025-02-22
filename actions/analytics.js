06628007db380ddd35b09c6ddbd6637e2805d569
mittir <akashjeet9038@gmail.com>
2025-02-22 23:33:00 +0530

    update mood-analytics

--- actions/analytics.js
+++ actions/analytics.js
@@ -12,4 +12,20 @@ export async function getAnalytics(period = "30d") {
     });
 
     if(!user) throw new Error("User not found");
+
+    const startDate = new Date()
+    switch (period) {
+        case "7d":
+            startDate.setDate(startDate.getDate() - 7);
+            break;
+        case "15d":
+            startDate.setDate(startDate.getDate() - 15);
+            break;
+        case "30d":
+        default:
+            startDate.setDate(startDate.getDate() - 30);
+            break;
+    }
+
+    const entries = await
 }
