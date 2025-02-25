2853673f381369e6a43fbc268714fa740dff6c90
mittir <akashjeet9038@gmail.com>
2025-02-25 14:48:00 +0530

    fetch journal entries by collectionId

--- app/(main)/collection/[collectionId]/page.jsx
+++ app/(main)/collection/[collectionId]/page.jsx
@@ -1,7 +1,9 @@
+import { getJournalEntries } from '@/actions/journal';
 import React from 'react'
 
 const CollectionPage = async({params}) => {
     const {collectionId} = params;
+    const entries = await getJournalEntries({ collectionId });
   return (
     <div>CollectionPage</div>
   )
