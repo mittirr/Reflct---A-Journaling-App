6ab9eef95ae0b4b1a6ffa56a70662c5edc4caa61
mittir <akashjeet9038@gmail.com>
2025-03-05 16:12:36 +0530

    update delete-collection

--- app/(main)/collection/_components/delete-collection.jsx
+++ app/(main)/collection/_components/delete-collection.jsx
@@ -1,8 +1,76 @@
-import React from 'react'
+"use client"
 
-const DeleteCollectionDialog = () => {
+import { deleteCollection } from '@/actions/collection';
+import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from '@/components/ui/alert-dialog'
+import { Button } from '@/components/ui/button';
+import useFetch from '@/hooks/use-fetch';
+import { Trash2 } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+import React, { useState, useEffect} from 'react'
+import { toast } from 'sonner';
+
+const DeleteCollectionDialog = ({collection, entriesCount = 0}) => {
+  const router = useRouter();
+  const [open, setOpen] = useState(false);
+
+const {
+  loading: isDeleting,
+  fn: deleteCollectionFn,
+  data: deletedCollection,
+} = useFetch(deleteCollection);
+
+useEffect(() => {
+  if(deleteCollection && !isDeleting){
+    setOpen(false);
+    toast.error(
+      `Collection "${collection.name}" and all its entries deleted`
+    );
+
+    router.push("/dashboard");
+  }
+}, [deletedCollection, isDeleting]);
+
+const handleDelete = () => {
+  deleteCollectionFn(collection.id);
+};
+
   return (
-    <div>DeleteCollectionDialog</div>
+    <AlertDialog open={open} onOpenChange={setOpen}>
+      <AlertDialogTrigger asChild>
+        <Button variant="destructive" size="sm">
+          <Trash2 className="h-4 w-4 mr-2"/>
+          Delete
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>
+            Delete &quot;{collection.name}&quot;?
+          </AlertDialogTitle>
+          <div className="space-y-2 text-muted-foreground text-sm">
+            <p>This will permanently delete:</p>
+            <ul className="list-disc list-inside">
+              <li>The collection &quot;{collection.name}&quot;</li>
+              <li>
+                {entriesCount} journal{" "}
+                {entriesCount === 1 ? "entry" : "entries"}
+              </li>
+            </ul>
+            <p className="font-semibold text-red-600">
+              This action cannot be undone.
+            </p>
+          </div>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <Button
+            onClick={handleDelete}
+            className="bg-red-500 hover:bg-red-600"
+            disabled={isDeleting}
+          >{isDeleting? "Deleting..." : "Delete Collection"}</Button>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   )
 }
 
