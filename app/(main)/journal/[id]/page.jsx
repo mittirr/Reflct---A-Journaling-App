91cc52106e2a0d2499f5badfaa7f57e70834f153
mittir <akashjeet9038@gmail.com>
2025-03-06 13:36:12 +0530

    update journal id page

---
+++ app/(main)/journal/[id]/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const JournalEntryPage = ({params}) => {
+    const { id } = params;
+    
+  return (
+    <div>JournalEntryPage</div>
+  )
+}
+
+export default JournalEntryPage
