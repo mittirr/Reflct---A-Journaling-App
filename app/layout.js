962b46408519cc48b382802859945b2525af0cda
mittir <akashjeet9038@gmail.com>
2025-03-08 17:10:01 +0530

    add analytics to the site

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import "./globals.css";
 import {Inter} from "next/font/google";
 import Header from "@/components/header";
 import {ClerkProvider} from "@clerk/nextjs";
+import { Analytics } from "@vercel/analytics/react"
 import { Toaster } from "sonner";
 const inter = Inter({ 
   subset: ['latin'],
@@ -24,7 +25,10 @@ export default function RootLayout({ children }) {
         <body className={`${inter.className}`}>
           <div className="bg-[url('/graph-paper.svg')] opacity-20 fixed -z-10 inset-0 bg-yellow-200" />
           <Header/>
-          <main className='min-h-screen'>{children}</main>
+          <main className='min-h-screen'>
+            {children}
+            <Analytics/>
+          </main>
           <Toaster richColors/>
           <footer className='bg-yellow-500 py-12 bg-opacity-20'>
             <div className='mx-auto px-4 text-center font-medium text-gray-700'>
