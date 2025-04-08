1e670a70a417e990b9667512e3495c88e28788ed
anishsaha46 <anish.ece321046@bppimt.ac.in>
2025-04-08 10:14:46 +0530

    feat: replace Clerk sign-up component with Kinde RegisterLink in SignUpPage

--- app/(auth)/sign-up/[[...sign-up]]/page.jsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.jsx
@@ -1,9 +1,15 @@
-import { SignUp } from '@clerk/nextjs'
+import { RegisterLink } from '@kinde-oss/kinde-auth-nextjs/components'
 import React from 'react'
 
 const SignUpPage = () => {
   return (
-    <div><SignUp/></div>
+    <div className="flex min-h-screen items-center justify-center">
+      <RegisterLink>
+        <button className="rounded-md bg-black px-4 py-2 text-sm font-medium text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-black focus:ring-offset-2">
+          Sign Up
+        </button>
+      </RegisterLink>
+    </div>
   )
 }
 
